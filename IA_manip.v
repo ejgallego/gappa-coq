38b3f1197f120ec0b3f78f3049d733ff279b28bf
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2005-01-24 18:02:10 +0000

    Il manquait un bout...

--- IA_manip.v
+++ IA_manip.v
@@ -47,5 +47,16 @@ exact Hz.
 ring.
 Qed.
 
+Theorem mul_decomposition_full_right :
+ forall a b c d : R,
+ forall ai bi ci di zi : FF,
+ IintF ai a -> IintF bi b -> IintF ci c -> IintF di d ->
+ IintF zi (c * (b - d) + d * (a - c) + (a - c) * (b - d))%R -> IintF zi (a * b - c * d)%R.
+intros a b c d ai bi ci di zi Ha Hb Hc Hd Hz.
+replace (a * b - c * d)%R with (c * (b - d) + d * (a - c) + (a - c) * (b - d))%R.
+exact Hz.
+ring.
+Qed.
+
 
 End IA_manip.
