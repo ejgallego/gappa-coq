5e922cd22dea8ad3ef4009516c06c6cfe851d7ff
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2005-04-17 21:03:48 +0000

    Nouvelles regles.

--- IA_manip.v
+++ IA_manip.v
@@ -47,6 +47,31 @@ exact Hz.
 ring.
 Qed.
 
+Definition sub_decomposition_rounded_left :=
+ absolute_transitivity.
+
+Theorem mul_decomposition_rounded_right :
+ forall a b c : R, forall zi : FF,
+ IintF zi (a * (b - c) + a * c) ->
+ true = true ->
+ IintF zi (a * b).
+intros a b c zi Hz _.
+replace (a * b)%R with (a * (b - c) + a * c)%R.
+exact Hz.
+ring.
+Qed.
+
+Theorem mul_decomposition_factor_left :
+ forall a b c : R, forall zi : FF,
+ IintF zi (a * (b - c)) ->
+ true = true ->
+ IintF zi (a * b - a * c).
+intros a b c zi Hz _.
+replace (a * b - a * c)%R with (a * (b - c))%R.
+exact Hz.
+ring.
+Qed.
+
 Theorem mul_decomposition_simple :
  forall a b c d : R, forall zi : FF,
  IintF zi (a * (b - d) + d * (a - c)) ->
