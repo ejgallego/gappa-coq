c53dc7c972ac9c9ef5f34d134cb7299ac424216a
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2005-10-14 17:46:27 +0000

    Nettoyage de l'arrondi

--- IA_float.v
+++ IA_float.v
@@ -3,43 +3,54 @@ Require Import F_rnd.
 
 Section IA_float.
 
-Axiom rounding_f : (float -> float) -> R -> R.
-Axiom rounding_f_correct_l :
- forall rnd : float -> float,
- forall x : R, forall y : float,
- (y <= x)%R ->
- (rnd y <= rounding_f rnd x)%R.
-Axiom rounding_f_correct_r :
- forall rnd : float -> float,
- forall x : R, forall y : float,
- (x <= y)%R ->
- (rounding_f rnd x <= rnd y)%R.
+Record rounding_operator : Set := mk_rounding {
+ rounding_real : R -> R;
+ rounding_float : float -> float;
+ rounding_coerc : forall f : float,
+  rounding_real f = float2R (rounding_float f);
+ rounding_mono : forall x y : R,
+  (x <= y)%R -> (rounding_real x <= rounding_real y)%R
+}.
 
 Definition f_round_helper (rnd : float -> float) (xi zi : FF) :=
  Fle_b (lower zi) (rnd (lower xi)) &&
  Fle_b (rnd (upper xi)) (upper zi).
 
 Theorem f_round :
- forall rnd : float -> float,
+ forall rnd : rounding_operator,
  forall x : R, forall xi zi : FF,
  IintF xi x ->
- f_round_helper rnd xi zi = true ->
- IintF zi (rounding_f rnd x).
+ f_round_helper (rounding_float rnd) xi zi = true ->
+ IintF zi (rounding_real rnd x).
 intros rnd x xi zi Hx Hb.
 generalize (andb_prop _ _ Hb). clear Hb. intros (H1,H2).
 generalize (Fle_b_correct _ _ H1). clear H1. intro H1.
 generalize (Fle_b_correct _ _ H2). clear H2. intro H2.
 split ; unfold FF2RR ; simpl.
 apply Rle_trans with (1 := H1).
-apply rounding_f_correct_l.
-apply (proj1 Hx).
+rewrite <- (rounding_coerc rnd (lower xi)).
+apply rounding_mono with (1 := proj1 Hx).
 apply Rle_trans with (2 := H2).
-apply rounding_f_correct_r.
-apply (proj2 Hx).
+rewrite <- (rounding_coerc rnd (upper xi)).
+apply rounding_mono with (1 := proj2 Hx).
 Qed.
 
-Definition rounding_float_ne (p: nat) (e: N) := rounding_f (rndNE p e).
-Definition float_round_ne (p: nat) (e: N) := f_round (rndNE p e).
+Parameter dummy_rounding_ne_real : nat -> N -> R -> R.
+Parameter dummy_rounding_ne_coerc :
+ forall p : nat, forall e : N, forall f : float,
+ dummy_rounding_ne_real p e f = float2R (rndNE p e f).
+Parameter dummy_rounding_ne_mono :
+ forall p : nat, forall e : N, forall x y : R,
+ (x <= y)%R ->
+ (dummy_rounding_ne_real p e x <= dummy_rounding_ne_real p e y)%R.
+Definition rounding_ne (p : nat) (e : N) :=
+ mk_rounding (dummy_rounding_ne_real p e) (rndNE p e)
+  (dummy_rounding_ne_coerc p e) (dummy_rounding_ne_mono p e).
+
+Definition rounding_float_ne (p: nat) (e: N) :=
+ rounding_real (rounding_ne p e).
+Definition float_round_ne (p: nat) (e: N) :=
+ f_round (rounding_ne p e).
 
 Theorem float_absolute_ne:
  forall p: nat, forall e: N,
@@ -71,22 +82,5 @@ Axiom sterbenz :
  A -> B -> C -> D ->
  true = true ->
  IintF zi z.
-
-Parameter add22_float64: R -> R -> R.
-Axiom add22_float64_round:
- forall x y : R, forall wi zi : FF,
- IintF wi (x + y) ->
- true = true ->
- IintF zi (add22_float64 x y).
-Axiom add22_float64_error:
- forall x y : R, forall wi zi : FF,
- IintF wi (Rabs (x + y)) ->
- true = true ->
- IintF zi (((add22_float64 x y) - (x + y)) / (x + y)).
-Axiom add22_float64_error_inv:
- forall x y : R, forall wi zi : FF,
- IintF wi (Rabs (add22_float64 x y)) ->
- true = true ->
- IintF zi (((add22_float64 x y) - (x + y)) / (x + y)).
 
 End IA_float.
