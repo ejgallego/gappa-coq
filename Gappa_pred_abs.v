842546dc3505ede50321b8abc8b6fcd359ea03a8
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2006-03-01 19:04:04 +0000

    Les inegalites triangulaires sont mes amies...

--- Gappa_pred_abs.v
+++ Gappa_pred_abs.v
@@ -106,4 +106,140 @@ apply refl_equal.
 apply abs_not_zero_correct with (1 := Hy) (2 := Hb).
 Qed.
 
+Definition add_aa_p_helper (xi yi zi : FF) :=
+ Fpos0 (lower zi) &&
+ Fle2 (lower zi) (Fminus2 (lower xi) (upper yi)) &&
+ Fle2 (Fplus2 (upper xi) (upper yi)) (upper zi).
+
+Theorem add_aa_p :
+ forall x y : R, forall xi yi zi : FF,
+ ABS x xi -> ABS y yi ->
+ add_aa_p_helper xi yi zi = true ->
+ ABS (x + y) zi.
+intros x y xi yi zi Hx Hy Hb.
+generalize (andb_prop _ _ Hb). clear Hb. intros (Hb,H3).
+generalize (andb_prop _ _ Hb). clear Hb. intros (H1,H2).
+generalize (Fpos0_correct _ H1). clear H1. intro H1.
+generalize (Fle2_correct _ _ H2). rewrite Fminus2_correct. clear H2. intro H2.
+generalize (Fle2_correct _ _ H3). rewrite Fplus2_correct. clear H3. intro H3.
+split.
+exact H1.
+split.
+apply Rle_trans with (Rabs x - Rabs y)%R.
+apply Rle_trans with (1 := H2).
+unfold Rminus.
+apply Rplus_le_compat with (1 := proj1 (proj2 Hx)).
+apply Ropp_le_contravar with (1 := proj2 (proj2 Hy)).
+replace (x + y)%R with (x - -y)%R. 2: ring.
+rewrite<- (Rabs_Ropp y).
+apply Rabs_triang_inv.
+apply Rle_trans with (Rabs x + Rabs y)%R.
+apply Rabs_triang.
+apply Rle_trans with (2 := H3).
+apply Rplus_le_compat with (1 := proj2 (proj2 Hx)) (2 := proj2 (proj2 Hy)).
+Qed.
+
+Definition add_aa_o_helper (xi yi zi : FF) :=
+ Fis0 (lower zi) &&
+ Fle2 (Fplus2 (upper xi) (upper yi)) (upper zi).
+
+Theorem add_aa_o :
+ forall x y : R, forall xi yi zi : FF,
+ ABS x xi -> ABS y yi ->
+ add_aa_o_helper xi yi zi = true ->
+ ABS (x + y) zi.
+intros x y xi yi zi Hx Hy Hb.
+generalize (andb_prop _ _ Hb). clear Hb. intros (H1,H2).
+generalize (Fis0_correct _ H1). clear H1. intro H1.
+generalize (Fle2_correct _ _ H2). rewrite Fplus2_correct. clear H2. intro H2.
+split.
+auto with real.
+split.
+replace (Gappa_real.lower zi) with (float2R (lower zi)). 2: trivial.
+rewrite H1.
+apply Rabs_pos.
+apply Rle_trans with (Rabs x + Rabs y)%R.
+apply Rabs_triang.
+apply Rle_trans with (2 := H2).
+apply Rplus_le_compat with (1 := proj2 (proj2 Hx)) (2 := proj2 (proj2 Hy)).
+Qed.
+
+Definition add_aa_n_helper (xi yi zi : FF) :=
+ Fpos0 (lower zi) &&
+ Fle2 (lower zi) (Fminus2 (lower yi) (upper xi)) &&
+ Fle2 (Fplus2 (upper xi) (upper yi)) (upper zi).
+
+Theorem add_aa_n :
+ forall x y : R, forall xi yi zi : FF,
+ ABS x xi -> ABS y yi ->
+ add_aa_n_helper xi yi zi = true ->
+ ABS (x + y) zi.
+intros x y xi yi zi Hx Hy Hb.
+generalize (andb_prop _ _ Hb). clear Hb. intros (Hb,H3).
+generalize (andb_prop _ _ Hb). clear Hb. intros (H1,H2).
+generalize (Fpos0_correct _ H1). clear H1. intro H1.
+generalize (Fle2_correct _ _ H2). rewrite Fminus2_correct. clear H2. intro H2.
+generalize (Fle2_correct _ _ H3). rewrite Fplus2_correct. clear H3. intro H3.
+split.
+exact H1.
+split.
+apply Rle_trans with (Rabs y - Rabs x)%R.
+apply Rle_trans with (1 := H2).
+unfold Rminus.
+apply Rplus_le_compat with (1 := proj1 (proj2 Hy)).
+apply Ropp_le_contravar with (1 := proj2 (proj2 Hx)).
+replace (x + y)%R with (y - -x)%R. 2: ring.
+rewrite<- (Rabs_Ropp x).
+apply Rabs_triang_inv.
+apply Rle_trans with (Rabs x + Rabs y)%R.
+apply Rabs_triang.
+apply Rle_trans with (2 := H3).
+apply Rplus_le_compat with (1 := proj2 (proj2 Hx)) (2 := proj2 (proj2 Hy)).
+Qed.
+
+Theorem sub_aa_p :
+ forall x y : R, forall xi yi zi : FF,
+ ABS x xi -> ABS y yi ->
+ add_aa_p_helper xi yi zi = true ->
+ ABS (x - y) zi.
+intros x y xi yi zi Hx Hy Hb.
+unfold Rminus.
+apply add_aa_p with xi yi.
+exact Hx.
+unfold ABS.
+rewrite Rabs_Ropp.
+exact Hy.
+exact Hb.
+Qed.
+
+Theorem sub_aa_o :
+ forall x y : R, forall xi yi zi : FF,
+ ABS x xi -> ABS y yi ->
+ add_aa_o_helper xi yi zi = true ->
+ ABS (x - y) zi.
+intros x y xi yi zi Hx Hy Hb.
+unfold Rminus.
+apply add_aa_o with xi yi.
+exact Hx.
+unfold ABS.
+rewrite Rabs_Ropp.
+exact Hy.
+exact Hb.
+Qed.
+
+Theorem sub_aa_n :
+ forall x y : R, forall xi yi zi : FF,
+ ABS x xi -> ABS y yi ->
+ add_aa_n_helper xi yi zi = true ->
+ ABS (x - y) zi.
+intros x y xi yi zi Hx Hy Hb.
+unfold Rminus.
+apply add_aa_n with xi yi.
+exact Hx.
+unfold ABS.
+rewrite Rabs_Ropp.
+exact Hy.
+exact Hb.
+Qed.
+
 End Gappa_pred_abs.
