3da3be1ba252a2583b744c2bec1cf3a2c3cf35e6
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2006-06-26 14:58:48 +0000

    Dichotomie sur intervalles simplifies.

--- Gappa_library.v
+++ Gappa_library.v
@@ -6,3 +6,6 @@ Require Export Gappa_pred_fixflt.
 Require Export Gappa_rewriting.
 
 Ltac finalize := exact (refl_equal true).
+Ltac next_interval t h :=
+ apply t with (1 := h) ; [ finalize |
+ clear h ; intro h ; simpl in h ; generalize h ; clear h ].
