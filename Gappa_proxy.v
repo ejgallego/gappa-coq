221cea09745c285997e4a929586ff91b83ae16ae
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2006-08-30 17:16:09 +0000

    Fonctions booleennes correctes, peut-etre.

--- Gappa_proxy.v
+++ Gappa_proxy.v
@@ -1,19 +1,21 @@
 Require Import Gappa_common.
+Require Import Gappa_round.
 
 Section Gappa_proxy.
 
 Theorem fix_of_flt_bnd :
- forall x : R, forall xi : FF, forall m : Z, forall n : positive,
- FLT x n -> ABS x xi ->
- true = true ->
- FIX x m.
+ forall x : R, forall xi : FF, forall n : Z, forall p : positive,
+ FLT x p -> ABS x xi ->
+ Zle_bool (n + Zpos p) (Zpos (digits (pos_of_Z (Fnum (lower xi)))) + Fexp (lower xi))
+ && Fpos (lower xi) = true ->
+ FIX x n.
 Admitted.
 
 Theorem flt_of_fix_bnd :
- forall x : R, forall xi : FF, forall m : Z, forall n : positive,
- FIX x m -> ABS x xi ->
- true = true ->
- FLT x n.
+ forall x : R, forall xi : FF, forall n : Z, forall p : positive,
+ FIX x n -> ABS x xi ->
+ Zle_bool (Zpos (digits (pos_of_Z (Fnum (upper xi)))) + Fexp (upper xi)) (n + Zpos p) = true ->
+ FLT x p.
 Admitted.
 
 End Gappa_proxy.
