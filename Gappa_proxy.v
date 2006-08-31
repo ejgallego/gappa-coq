ca74f35093cbb716976d8a97c57330031063637e
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2006-08-31 17:29:18 +0000

    flt_of_fix_bnd

--- Gappa_proxy.v
+++ Gappa_proxy.v
@@ -11,11 +11,4 @@ Theorem fix_of_flt_bnd :
  FIX x n.
 Admitted.
 
-Theorem flt_of_fix_bnd :
- forall x : R, forall xi : FF, forall n : Z, forall p : positive,
- FIX x n -> ABS x xi ->
- Zle_bool (Zpos (digits (pos_of_Z (Fnum (upper xi)))) + Fexp (upper xi)) (n + Zpos p) = true ->
- FLT x p.
-Admitted.
-
 End Gappa_proxy.
