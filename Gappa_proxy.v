bca95ea21afdf92a63a2e75494b07dbf3d4764b3
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2007-02-02 11:04:22 +0000

    Quelques axiomes faux...

--- Gappa_proxy.v
+++ Gappa_proxy.v
@@ -1,3 +1,14 @@
+Require Import Gappa_common.
+
 Section Gappa_proxy.
+
+Axiom relative_add : forall (p : positive) (e : Z) (r1 r2 : R), R.
+Axiom relative_sub : forall (p : positive) (e : Z) (r1 r2 : R), R.
+Axiom relative_mul : forall (p : positive) (e : Z) (r1 r2 : R), R.
+Axiom relative_fma : forall (p : positive) (e : Z) (r1 r2 r3 : R), R.
+Axiom rel_round_add : forall (p : positive) (e : Z) (Q P : Prop), Q -> P.
+Axiom rel_round_sub : forall (p : positive) (e : Z) (Q P : Prop), Q -> P.
+Axiom rel_round_mul : forall (p : positive) (e : Z) (Q P : Prop), Q -> P.
+Axiom rel_round_fma : forall (p : positive) (e : Z) (Q P : Prop), Q -> P.
 
 End Gappa_proxy.
