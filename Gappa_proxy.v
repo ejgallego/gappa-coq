dfb2255d8e74be45f6d511df879c7296bdaabee4
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2007-10-24 16:47:00 +0000

    Axiomes pour rel_error.

--- Gappa_proxy.v
+++ Gappa_proxy.v
@@ -43,5 +43,9 @@ Axiom rel_round_add : forall (p : positive) (e : Z) (Q P : Prop), Q -> P.
 Axiom rel_round_sub : forall (p : positive) (e : Z) (Q P : Prop), Q -> P.
 Axiom rel_round_mul : forall (p : positive) (e : Z) (Q P : Prop), Q -> P.
 Axiom rel_round_fma : forall (p : positive) (e : Z) (Q P : Prop), Q -> P.
+Axiom rel_error_add : forall (p : positive) (e : Z) (Q P : Prop), Q -> P.
+Axiom rel_error_sub : forall (p : positive) (e : Z) (Q P : Prop), Q -> P.
+Axiom rel_error_mul : forall (p : positive) (e : Z) (Q P : Prop), Q -> P.
+Axiom rel_error_fma : forall (p : positive) (e : Z) (Q P : Prop), Q -> P.
 
 End Gappa_proxy.
