ad98548d36e74361ebcd163f097508aa464f08b2
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2009-04-21 19:04:03 +0000

    Added a dummy file for forcing Coq to prefix constants.

---
+++ Gappa_obfuscate.v
@@ -0,0 +1,51 @@
+(* This file forces Coq to use full locations to address Gappa constants with
+   the same names, hence avoiding potential conflicts with user constants. *)
+
+Definition Float1 := 0.
+Definition float2 := 0.
+Definition Float2 := 0.
+Definition float2R := 0.
+Definition float10 := 0.
+Definition Float10 := 0.
+Definition float10R := 0.
+Definition FF := 0.
+Definition makepairF := 0.
+Definition BND := 0.
+Definition ABS := 0.
+Definition REL := 0.
+Definition FIX := 0.
+Definition FLT := 0.
+Definition NZR := 0.
+Definition contradiction := 0.
+Definition rounding_float := 0.
+Definition rounding_fixed := 0.
+Definition roundZR := 0.
+Definition roundAW := 0.
+Definition roundUP := 0.
+Definition roundDN := 0.
+Definition roundOD := 0.
+Definition roundNE := 0.
+Definition roundNO := 0.
+Definition roundNZ := 0.
+Definition roundNA := 0.
+Definition roundNU := 0.
+Definition roundND := 0.
+
+Definition R := 0.
+Definition Rle := 0.
+Definition Ropp := 0.
+Definition Rplus := 0.
+Definition Rminus := 0.
+Definition Rmult := 0.
+Definition Rdiv := 0.
+Definition Rabs := 0.
+Definition sqrt := 0.
+Definition R0 := 0.
+Definition R1 := 0.
+
+Definition xH := 0.
+Definition xI := 0.
+Definition xO := 0.
+Definition Zpos := 0.
+Definition Zneg := 0.
+Definition Z0 := 0.
