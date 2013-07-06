8a6a8ad02b9408328248cd717ed072ac1cb16620
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2013-07-06 23:57:49 +0200

    Fix shadowing of subset.

--- src/Gappa_library.v
+++ src/Gappa_library.v
@@ -3,6 +3,7 @@ Require Export Rbasic_fun.
 Require Export R_sqrt.
 Require Export Bool.
 Require Export Gappa_definitions.
+Require Export Gappa_tree.
 Require Export Gappa_pred_bnd.
 Require Export Gappa_pred_abs.
 Require Export Gappa_pred_fixflt.
@@ -12,7 +13,6 @@ Require Export Gappa_rewriting.
 Require Export Gappa_round_def.
 Require Export Gappa_fixed.
 Require Export Gappa_float.
-Require Export Gappa_tree.
 Require Export Gappa_user.
 
 Ltac finalize := vm_cast_no_check (refl_equal true).
